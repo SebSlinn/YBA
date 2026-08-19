6fff3d11a6799c502487fa9ff5e148c9b63e4298
SebSlinn <familyslinn@gmail.com>
2026-08-19 13:40:39 +0100

    Add Dockerfile and frontend service for deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,13 +3,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   images: {
-      unoptimized: process.env.NODE_ENV !== 'production',
-  // ...
+    unoptimized: process.env.NODE_ENV !== 'production',
     remotePatterns: [
       {
         protocol: "http",
         hostname: "localhost",
+        port: "8055",
+        pathname: "/assets/**",
+      },
+      {
+        protocol: "http",
+        hostname: "198.244.232.142",
         port: "8055",
         pathname: "/assets/**",
       },
